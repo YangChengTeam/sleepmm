76f19232e5613f6d243acb8c0c05c9585ca785a0
2037097758@qq.com <2037097758@qq.com>
2018-01-24 15:35:56 +0800

    增你分类

--- app/src/main/java/com/yc/sleepmm/base/ui/activity/LoginGroupActivity.java
+++ app/src/main/java/com/yc/sleepmm/base/ui/activity/LoginGroupActivity.java
@@ -67,14 +67,6 @@ public class LoginGroupActivity extends BaseActivity implements LoginContract.Vi
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //顶部透明
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-        }
         mLoginPresenter = new LoginPresenter(this);
         mLoginPresenter.attachView(this);
     }
