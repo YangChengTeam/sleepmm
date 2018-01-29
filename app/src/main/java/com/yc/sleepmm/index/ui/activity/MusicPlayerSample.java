0b313a228f6077b957a5a84c3006f7e7d0e7ced2
Yuye584312311 <584312311@qq.com>
2018-01-29 08:48:27 +0800

    样例代码微调

--- app/src/main/java/com/yc/sleepmm/index/ui/activity/MusicPlayerSample.java
+++ app/src/main/java/com/yc/sleepmm/index/ui/activity/MusicPlayerSample.java
@@ -138,10 +138,8 @@ public class MusicPlayerSample extends AppCompatActivity implements OnUserPlayer
                     for (MediaMusicCategoryList.DataBean dataBean : data.getData()) {
                         MusicInfo musicInfo=new MusicInfo();
                         musicInfo.setMusicID(dataBean.getId());
-                        musicInfo.setMusicAlbum(dataBean.getTitle());
                         musicInfo.setMusicTitle(dataBean.getTitle());
                         musicInfo.setMusicDurtion(dataBean.getSeconds());
-                        musicInfo.setMsuicAlbumIcon(dataBean.getCover());
                         musicInfo.setMusicCover(dataBean.getCover());
                         musicInfo.setMusicAuthor(dataBean.getAuthor());
                         musicInfo.setMusicAlbumTitle(dataBean.getTitle());
